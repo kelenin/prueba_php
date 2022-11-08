dbd741103f9c4b1c5583a2e1145190084eb5da7a
kelenin <katherensalom@gmail.com>
2022-11-08 12:50:35 -0400

    modificacion

---
+++ tareatxt.php
@@ -0,0 +1,26 @@
+<?php
+
+
+    $archivo = fopen("tmptxt/archivo.txt", "w+");
+    $contenido = "Total de tareas:\n ";
+    for($i=0;$i < 1000000; $i++)
+    {
+        $digit = rand(0, 6);
+    }
+    $contenido.= $digit."\n \n \n \n";
+
+    $contenido.= "Tiempo que toma cada tarea: "."\n \n";
+    for($i=0;$i < 6; $i++)
+    {
+        
+        $start_time = microtime(true);
+        $tiempo_final = microtime(true);
+        $end_time = microtime(true);
+        $duration = $end_time - $start_time;
+
+        $contenido.= $i.",".$duration."\n";
+    }
+    fwrite($archivo, PHP_EOL ."$contenido");
+    fclose($archivo);
+
+?>
